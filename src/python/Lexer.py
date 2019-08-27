38d0c738235f35f9ad52f13ae041b56be1cf0491
Stacks-Mamba <stexor12@gmail.com>
2019-08-27 10:23:06 +0100

    More changes

--- src/python/Lexer.py
+++ src/python/Lexer.py
@@ -60,7 +60,7 @@ class Token:
 
 
 class Lexer:
-    def __init__():
+    def __init__(self):
         self.line = 0
         self.col = 0
         self.src_file = None
@@ -68,12 +68,12 @@ class Lexer:
         self.fp = None
 
     #Add and open source file
-    def add_src(file):
+    def add_src(self,file):
         self.src_file = file
         self.fp = open(file,"r")
 
     #Function used for getting tokens and adding them to the symbol
-    def getToken():
+    def getToken(self):
         #Get a line from the file
         self.buffer = self.fp.readline()
         self.line+=1
