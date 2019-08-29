3b2fbd8766069fc64e19a755872744ec5a55207c
Stacks-Mamba <stexor12@gmail.com>
2019-08-29 22:20:30 +0100

    more changes

--- src/python/Token.py
+++ src/python/Token.py
@@ -16,19 +16,26 @@ class TokenType(Enum):
     ".":"TOK_PONTO",";":"TOK_PV",":":"TOK_DP","..":"TOK_ELIP","^":"TOK_CIRC"}
 
 
-    PRE_ID = {}
+    KEYWORDS = { "and":"TOK_AND","array":"TOK_ARRAY","begin":"TOK_BEGIN",
+    "case":"TOK_CASE","const":"TOK_CONST","div":"TOK_DIV","do":"TOK_DO",
+    "downto":"TOK_DOWNTO","else":"TOK_ELSE","file":"TOK_FILE",
+    "for":"TOK_FOR","forward":"TOK_FORWARD","function":"TOK_FUNCTION",
+    "goto":"TOK_GOTO","if":"TOK_IF","in":"TOK_IN",
+    "label":"TOK_LABEL","main":"TOK_MAIN","mod":"TOK_MOD",
+    "nil":"TOK_NIL","not":"TOK_NOT","of":"TOK_OF",
+    "or":"TOK_OR","packed":"TOK_PACKED","procedure":"TOK_PROCEDURE",
+    "program":"TOK_PROGRAM","record":"TOK_RECORD","repeat":"TOK_REPEAT",
+    "set":"TOK_SET","thento":"TOK_THENTO","type":"TOK_TYPE",
+    "until":"TOK_UNTIL","var":"TOK_VAR","while":"TOK_WHILE",
+    "with":"TOK_WITH","define":"TOK_DEFINE","extern":"TOK_EXTERN",
+    "external":"TOK_EXTERNAL","module":"TOK_MODULE","otherwise":"TOK_OTHERWISE",
+    "private":"TOK_PRIVATE","public":"TOK_PUBLIC","static":"TOK_STATIC",
+    "univ":"TOK_UNIV"}
 
 
 
 
 class Token:
-    RESERVED_WORDS = ["and","array","begin","case","const","div","do"
-                ,"downto","else","file","for","forward","function",
-                "goto","if","in","label","main","mod","nil","not","of","or",
-                "packed","procedure","program","record","repeat","set","then"
-                "to","type","until","var","while","with","define","extern",
-                "external","module","otherwise","private","public","static"
-                "univ"]
 
     def __init__(value,lexeme):
         self.value = value
