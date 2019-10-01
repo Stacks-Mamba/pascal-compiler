fdf0c398aee0f12dbeaa3eb2e29c38c4342f0857
Hairton <hairton@outlook.com>
2019-10-01 21:05:17 +0100

    All Simple functions Implemented

--- src/java/compilador/isptec/Sintatico/AnalisadorSintatico.java
+++ src/java/compilador/isptec/Sintatico/AnalisadorSintatico.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package compilador.isptec.sintatico;
+﻿package compilador.isptec.sintatico;
 import compilador.isptec.lexico.Analex;
 import compilador.isptec.lexico.Tokens;
 import compilador.isptec.lexico.Token;
@@ -35,13 +30,108 @@ public class AnalisadorSintatico {
     {
         
     }
-    private void label(){
-    if(lookahead.getToken().equals(Tokens.NUMINT))
+    private void emptystatement(){
+        empty();
+    }
+    private void label()
     {
-        
+        if(lookahead.getToken().equals(Tokens.NUMINT))
+        {
+            consume(Tokens.NUMINT);
+        }
     }
+    private void fieldIdentifier()
+    {
+        if(lookahead.getToken().equals(Tokens.ID))
+        {
+            consume(Tokens.ID);
+        }
     }
-     private void consume(Tokens token)
+    private void controlVariable()
+    {
+        if(lookahead.getToken().equals(Tokens.ID))
+        {
+            consume(Tokens.ID);
+        }
+    }
+    private void functionIdentifier()
+    {
+        if(lookahead.getToken().equals(Tokens.ID))
+        {
+            consume(Tokens.ID);
+        }
+    }
+    private void procedureIdentifier()
+    {
+        if(lookahead.getToken().equals(Tokens.ID))
+        {
+            consume(Tokens.ID);
+        }
+    }
+    private void variableIdentifier()
+    {
+        if(lookahead.getToken().equals(Tokens.ID))
+        {
+            consume(Tokens.ID);
+        }
+    }
+    private void constantIdentifier()
+    {
+        if(lookahead.getToken().equals(Tokens.ID))
+        {
+            consume(Tokens.ID);
+        }
+    }
+    private void addingOperator()
+    {
+     if(lookahead.getToken().equals(Tokens.MAIS))
+         consume(Tokens.MAIS);
+     if(lookahead.getToken().equals(Tokens.MENOS))
+         consume(Tokens.MENOS);
+     if(lookahead.getToken().equals(Tokens.OR))
+         consume(Tokens.OR);
+    }
+    private void relationalOperator()
+    {
+     if(lookahead.getToken().equals(Tokens.IGUAL))
+         consume(Tokens.IGUAL);
+     if(lookahead.getToken().equals(Tokens.DIFERENTE))
+         consume(Tokens.DIFERENTE);
+     if(lookahead.getToken().equals(Tokens.MENOR))
+         consume(Tokens.MENOR);
+     if(lookahead.getToken().equals(Tokens.MENORIGUAL))
+         consume(Tokens.MENORIGUAL);
+     if(lookahead.getToken().equals(Tokens.MAIORIGUAL))
+         consume(Tokens.MAIORIGUAL);
+     if(lookahead.getToken().equals(Tokens.MAIOR))
+         consume(Tokens.MAIOR);
+     if(lookahead.getToken().equals(Tokens.IN))
+         consume(Tokens.IN);
+    }
+    private void multiplyingOperator()
+    {
+     if(lookahead.getToken().equals(Tokens.VEZES))
+         consume(Tokens.VEZES);
+     if(lookahead.getToken().equals(Tokens.DIVISAO))
+         consume(Tokens.DIVISAO);
+     if(lookahead.getToken().equals(Tokens.DIV))
+         consume(Tokens.DIV);
+     if(lookahead.getToken().equals(Tokens.MOD))
+         consume(Tokens.MOD);
+     if(lookahead.getToken().equals(Tokens.AND))
+         consume(Tokens.AND);
+     if(lookahead.getToken().equals(Tokens.MAIOR))
+         consume(Tokens.MAIOR);
+    }
+    
+    private void sign()
+    {
+     if(lookahead.getToken().equals(Tokens.MAIS))
+         consume(Tokens.MAIS);
+     if(lookahead.getToken().equals(Tokens.MENOS))
+         consume(Tokens.MENOS);
+    }
+    private void consume(Tokens token)
     {
         if(lookahead.getToken() == token)
         {
