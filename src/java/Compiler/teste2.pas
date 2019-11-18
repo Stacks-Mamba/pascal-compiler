8af042b51b21beaac30526bf9900aa97eb105df0
stacks-mamba <stexor12@gmail.com>
2019-11-19 00:35:31 +0100

    Finished organizing directories lul

--- src/java/Compiler/teste2.pas
+++
@@ -1,3 +0,0 @@
-program Fact_rec(output;test;um);
-begin
-end.
---
+++ src/pascalCompilerJava/src/compiler/main/Main.java
@@ -0,0 +1,8 @@
+package compiler.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
