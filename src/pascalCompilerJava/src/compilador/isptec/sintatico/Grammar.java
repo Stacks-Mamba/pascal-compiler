5854b0834a36617f405632ece3b8ba94e086d64f
stacks-mamba <stexor12@gmail.com>
2019-11-19 03:33:28 +0100

    more changes

--- src/pascalCompilerJava/src/compilador/isptec/sintatico/Grammar.java
+++ src/pascalCompilerJava/src/compilador/isptec/sintatico/Grammar.java
@@ -122,6 +122,18 @@ public class Grammar {
         }
     }
 
+    public static void sign(){
+        if(Parser.lookahead.getToken()==Tokens.MAIS)){
+            Parser.consume(Tokens.MAIS);
+        }
+        else if(Parser.lookahead.getToken()==Tokens.MENOS){
+            Parser.consume(Tokens.MENOS);
+        }
+        else{
+            Parser.error(Tokens.MAIS,Parser.UNKNOWN_ERROR);
+        }
+    }
+
     public static void typeDefPart(){
         if(Parser.lookahead.getToken()==Tokens.TYPE){
             Parser.consume(Tokens.TYPE);
@@ -145,6 +157,151 @@ public class Grammar {
         type();
     }
 
+    public static void type(){
 
-    
+        Tokens lookahead = Parser.lookahead.getToken();
+        if(lookahead==Tokens.ABREPAR){
+            scalarType();
+        }
+        else if(lookahead==Tokens.NUMINT || lookahead==Tokens.NUMREAL){
+            unsignedNumber();
+            Parser.consume(Tokens.PONTOPONTO);
+            constant();
+        }
+        else if(lookahead==Tokens.MAIS || lookahead==Tokens.MENOS){
+            sign();
+            sign1();
+            Parser.consume(Tokens.PONTOPONTO);
+            constant();
+        }
+        else if(lookahead==Tokens.STRING){
+            Parser.consume(Tokens.STRING);
+            Parser.consume(Tokens.PONTOPONTO);
+            constant();
+        }
+        else if(lookahead==Tokens.ID){
+            Parser.consume(Tokens.ID);
+            type1();
+        }
+        else if(lookahead==Tokens.ARRAY || lookahead==Tokens.RECORD
+                || lookahead ==Tokens.SET || lookahead == Tokens.FILE){
+            structuredType();
+        }
+    }
+
+    public static void type1(){
+        if(Parser.lookahead.getToken()==Tokens.PONTOPONTO){
+            Parser.consume(Tokens.PONTOPONTO);
+            constant();
+        }
+    }
+
+    public static void scalarType(){
+        Parser.consume(Tokens.ABREPAR);
+        Parser.consume(Tokens.ID);
+        while(Parser.lookahead.getToken()==Tokens.VIRGULA){
+            Parser.consume(Tokens.VIRGULA);
+            Parser.consume(Tokens.ID);
+        }
+        Parser.consume(Tokens.FECHAPAR);
+    }
+
+
+    public static void structuredType(){
+        Tokens lookahead = Parser.lookahead.getToken();
+        if(lookahead == Tokens.ARRAY){
+            arrayType();
+        }
+        else if(lookahead == Tokens.RECORD){
+            recordType();
+        }
+        else if (lookahead == Tokens.SET){
+            setType();
+        }
+        else if(lookahead == Tokens.FILE) {
+            fileType();
+        }
+        else{
+            Parser.error(Tokens.MAIS,Parser.UNKNOWN_ERROR);
+        }
+    }
+
+    public static void arrayType(){
+        Parser.consume(Tokens.ARRAY);
+        Parser.consume(Tokens.ABRERET);
+        indexType();
+        while(Parser.lookahead.getToken()==Tokens.VIRGULA){
+            Parser.consume(Tokens.VIRGULA);
+            indexType();
+        }
+        Parser.consume(Tokens.FECHARET);
+        Parser.consume(Tokens.OF);
+        type();
+    }
+
+    public static void indexType(){
+        Tokens lookahead = Parser.lookahead.getToken();
+        if(lookahead == Tokens.ABREPAR){
+            scalarType();
+        }
+        else if(lookahead == Tokens.NUMINT || lookahead == Tokens.NUMREAL){
+            unsignedNumber();
+            Parser.consume(Tokens.PONTOPONTO);
+            constant();
+        }
+        else if(lookahead==Tokens.MAIS || lookahead==Tokens.MENOS){
+            sign();
+            sign1();
+            Parser.consume(Tokens.PONTOPONTO);
+            constant();
+        }
+        else if(lookahead==Tokens.STRING){
+            Parser.consume(Tokens.STRING);
+            Parser.consume(Tokens.PONTOPONTO);
+            constant();
+        }
+        else if(lookahead==Tokens.ID){
+            Parser.consume(Tokens.ID);
+            type1();
+        }
+        else{
+            Parser.error(Tokens.ABREPAR,Parser.UNKNOWN_ERROR);
+        }
+    }
+
+    public static void recordType(){
+        Parser.consume(Tokens.RECORD);
+        fieldList();
+        Parser.consume(Tokens.END);
+    }
+
+    public static void fieldList(){
+        Tokens lookahead = Parser.lookahead.getToken();
+        if(lookahead == Tokens.CASE){
+            variantType();
+        }
+        else{
+            fixedPart();
+            fixedPart1();
+        }
+    }
+
+    public static void fixedPart(){
+        Tokens lookahead = Parser.lookahead.getToken();
+        recordSection();
+        while(lookahead == Tokens.PONTOVIRGULA){
+            Parser.consume(Tokens.PONTOVIRGULA);
+            recordSection();
+        }
+    }
+
+
+    public static void fixedPart1(){
+        
+    }
+
+
+
+
+
 }
